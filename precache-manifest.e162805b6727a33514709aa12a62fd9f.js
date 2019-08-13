675967c020b5cb2407426f91971f75c76a238f59
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2019-08-13 14:11:58 -0700

    Updates

---
+++ precache-manifest.e162805b6727a33514709aa12a62fd9f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c461dd56c2bd60dece342bc85747674",
+    "url": "/jonathanyeew.github.io/index.html"
+  },
+  {
+    "revision": "d5ac5839c83967fe4c60",
+    "url": "/jonathanyeew.github.io/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a652002499c8dc586a33",
+    "url": "/jonathanyeew.github.io/static/css/main.8c9a1a8b.chunk.css"
+  },
+  {
+    "revision": "d5ac5839c83967fe4c60",
+    "url": "/jonathanyeew.github.io/static/js/2.e20d4a54.chunk.js"
+  },
+  {
+    "revision": "a652002499c8dc586a33",
+    "url": "/jonathanyeew.github.io/static/js/main.e419c8aa.chunk.js"
+  },
+  {
+    "revision": "85920605d101d1a34151",
+    "url": "/jonathanyeew.github.io/static/js/runtime~main.0930e606.js"
+  },
+  {
+    "revision": "6fd60b185ce99b2c7101e3febecf10a2",
+    "url": "/jonathanyeew.github.io/static/media/ControlledComponent.6fd60b18.png"
+  },
+  {
+    "revision": "745c0bd5223f6ee1bfbbbd77841b6fee",
+    "url": "/jonathanyeew.github.io/static/media/ControlledComponentTest.745c0bd5.png"
+  },
+  {
+    "revision": "0ca973166b6c333a870439228fd58f56",
+    "url": "/jonathanyeew.github.io/static/media/LearnUp.0ca97316.png"
+  },
+  {
+    "revision": "90615f2e1340e40b3e18f56090df4246",
+    "url": "/jonathanyeew.github.io/static/media/UncontrolledComponent.90615f2e.png"
+  },
+  {
+    "revision": "eadca2d271ef55eabe8b6efe31f148a3",
+    "url": "/jonathanyeew.github.io/static/media/UncontrolledComponentTest.eadca2d2.png"
+  },
+  {
+    "revision": "4c647ad6fe6fbb0e3a43bcbb2f1b85e3",
+    "url": "/jonathanyeew.github.io/static/media/Upsplash1.4c647ad6.png"
+  },
+  {
+    "revision": "4063361aec804e79cf27dab3426b92db",
+    "url": "/jonathanyeew.github.io/static/media/Upsplash2.4063361a.png"
+  },
+  {
+    "revision": "6bc19395c0c2e17aed3ba33bdd55cc20",
+    "url": "/jonathanyeew.github.io/static/media/Upsplash3.6bc19395.png"
+  },
+  {
+    "revision": "7d58e93ed8cb48008c249589183f799b",
+    "url": "/jonathanyeew.github.io/static/media/Upsplash4.7d58e93e.png"
+  },
+  {
+    "revision": "ebb492c474ce333ddb1d45c20ebf864f",
+    "url": "/jonathanyeew.github.io/static/media/datasaas.ebb492c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1e46dca16fa0aef0f71b6217de91918.js"
+  "/jonathanyeew.github.io/precache-manifest.e162805b6727a33514709aa12a62fd9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jonathanyeew.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
